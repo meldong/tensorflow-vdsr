18312ddf34c262ea8ac844722ec00617763a852d
Francis D <32295400+meldong@users.noreply.github.com>
2018-11-14 07:24:15 +0800

    Update aug_train.m

--- data/aug_train.m
+++ data/aug_train.m
@@ -1,15 +1,10 @@
-
-
-
-
-dataDir = '291';%fullfile('data', '291');
-mkdir('train');
-count = 0;
-f_lst = [];
-f_lst = [f_lst; dir(fullfile(dataDir, '*.jpg'))];
+dataDir = '291';
+f_lst = dir(fullfile(dataDir, '*.jpg'));
 f_lst = [f_lst; dir(fullfile(dataDir, '*.bmp'))];
+mkdir('train');
+
+count = 0;
 for f_iter = 1:numel(f_lst)
-%     disp(f_iter);
     f_info = f_lst(f_iter);
     if f_info.name == '.'
         continue;
@@ -17,29 +12,32 @@ for f_iter = 1:numel(f_lst)
     f_path = fullfile(dataDir,f_info.name);
     img_raw = imread(f_path);
     img_raw = rgb2ycbcr(img_raw);
+
     img_raw = im2double(img_raw(:,:,1));
-    
+
     img_size = size(img_raw);
     width = img_size(2);
     height = img_size(1);
-    
-    img_raw = img_raw(1:height-mod(height,12),1:width-mod(width,12),:);
-    
+
+    img_raw = img_raw(1:height - mod(height,12), 1:width - mod(width,12),:);
+
     img_size = size(img_raw);
+
+    img_2 = imresize(imresize(img_raw,1/2,'bicubic'),[img_size(1),img_size(2)],'bicubic');
+    img_3 = imresize(imresize(img_raw,1/3,'bicubic'),[img_size(1),img_size(2)],'bicubic');
+    img_4 = imresize(imresize(img_raw,1/4,'bicubic'),[img_size(1),img_size(2)],'bicubic');
+
     patch_size = 41;
     stride = 41;
     x_size = (img_size(2)-patch_size)/stride+1;
     y_size = (img_size(1)-patch_size)/stride+1;
-    
-    img_2 = imresize(imresize(img_raw,1/2,'bicubic'),[img_size(1),img_size(2)],'bicubic');
-    img_3 = imresize(imresize(img_raw,1/3,'bicubic'),[img_size(1),img_size(2)],'bicubic');
-    img_4 = imresize(imresize(img_raw,1/4,'bicubic'),[img_size(1),img_size(2)],'bicubic');
-    
+
     for x = 0:x_size-1
         for y = 0:y_size-1
-            x_coord = x*stride; y_coord = y*stride; 
+            x_coord = x*stride; y_coord = y*stride;
+
+            % round 1
             patch_name = sprintf('train/%d',count);
-            
             patch = imrotate(img_raw(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 0);
             save(patch_name, 'patch');
             patch = imrotate(img_2(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 0);
@@ -48,11 +46,10 @@ for f_iter = 1:numel(f_lst)
             save(sprintf('%s_3', patch_name), 'patch');
             patch = imrotate(img_4(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 0);
             save(sprintf('%s_4', patch_name), 'patch');
-            
             count = count+1;
-            
+
+            % round 2
             patch_name = sprintf('train/%d',count);
-            
             patch = imrotate(img_raw(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 90);
             save(patch_name, 'patch');
             patch = imrotate(img_2(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 90);
@@ -61,11 +58,10 @@ for f_iter = 1:numel(f_lst)
             save(sprintf('%s_3', patch_name), 'patch');
             patch = imrotate(img_4(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 90);
             save(sprintf('%s_4', patch_name), 'patch');
-            
             count = count+1;
             
+            % round 3
             patch_name = sprintf('train/%d',count);
-            
             patch = fliplr(imrotate(img_raw(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 0));
             save(patch_name, 'patch');
             patch = fliplr(imrotate(img_2(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 0));
@@ -74,11 +70,10 @@ for f_iter = 1:numel(f_lst)
             save(sprintf('%s_3', patch_name), 'patch');
             patch = fliplr(imrotate(img_4(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 0));
             save(sprintf('%s_4', patch_name), 'patch');
-            
             count = count+1;
-            
+
+            % round 4
             patch_name = sprintf('train/%d',count);
-            
             patch = fliplr(imrotate(img_raw(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 90));
             save(patch_name, 'patch');
             patch = fliplr(imrotate(img_2(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 90));
@@ -87,13 +82,11 @@ for f_iter = 1:numel(f_lst)
             save(sprintf('%s_3', patch_name), 'patch');
             patch = fliplr(imrotate(img_4(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 90));
             save(sprintf('%s_4', patch_name), 'patch');
-            
             count = count+1;
-            
-            
+
             %{
+            % round 5
             patch_name = sprintf('aug/%d',count);
-            
             patch = imrotate(img_raw(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 180);
             save(patch_name, 'patch');
             patch = imrotate(img_2(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 180);
@@ -102,11 +95,10 @@ for f_iter = 1:numel(f_lst)
             save(sprintf('%s_3', patch_name), 'patch');
             patch = imrotate(img_4(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 180);
             save(sprintf('%s_4', patch_name), 'patch');
-            
             count = count+1;
-            
+
+            % round 6
             patch_name = sprintf('aug/%d',count);
-            
             patch = fliplr(imrotate(img_raw(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 180));
             save(patch_name, 'patch');
             patch = fliplr(imrotate(img_2(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 180));
@@ -115,11 +107,10 @@ for f_iter = 1:numel(f_lst)
             save(sprintf('%s_3', patch_name), 'patch');
             patch = fliplr(imrotate(img_4(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 180));
             save(sprintf('%s_4', patch_name), 'patch');
-            
             count = count+1;
-            
+
+            % round 7
             patch_name = sprintf('aug/%d',count);
-            
             patch = imrotate(img_raw(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 270);
             save(patch_name, 'patch');
             patch = imrotate(img_2(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 270);
@@ -128,26 +119,21 @@ for f_iter = 1:numel(f_lst)
             save(sprintf('%s_3', patch_name), 'patch');
             patch = imrotate(img_4(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 270);
             save(sprintf('%s_4', patch_name), 'patch');
-            
             count = count+1;
-            
+
+            % round 8
             patch_name = sprintf('aug/%d',count);
-            
-            patch = fliplr(imrotate(img_raw(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 180));
+            patch = fliplr(imrotate(img_raw(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 270));
             save(patch_name, 'patch');
-            patch = fliplr(imrotate(img_2(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 180));
+            patch = fliplr(imrotate(img_2(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 270));
             save(sprintf('%s_2', patch_name), 'patch');
-            patch = fliplr(imrotate(img_3(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 180));
+            patch = fliplr(imrotate(img_3(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 270));
             save(sprintf('%s_3', patch_name), 'patch');
-            patch = fliplr(imrotate(img_4(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 180));
+            patch = fliplr(imrotate(img_4(y_coord+1:y_coord+patch_size,x_coord+1:x_coord+patch_size,:), 270));
             save(sprintf('%s_4', patch_name), 'patch');
-            
             count = count+1;
             %}
         end
     end
-    
-    display(count);
-    
-    
+    disp([num2str(f_iter), ' of ', num2str(numel(f_lst)), ' : ', num2str(count), ' images.']);
 end
